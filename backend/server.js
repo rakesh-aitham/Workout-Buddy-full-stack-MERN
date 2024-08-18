f6273f2009b6eb2d43143fe0729faf6545abc4c8
Rakesh <rakeshaitham1@gmail.com>
2024-08-18 22:11:02 +0530

    first-commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/workouts', workoutRoutes)
+app.use('/user', userRoutes)
+
+//connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('connected to database')
+        // listen to port
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port 4000...')
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
